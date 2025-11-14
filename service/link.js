b3ef014b99fb9612c35a466778bbbb969b57b75d
mifuo <110472388+mifuo@users.noreply.github.com>
2025-11-14 13:46:26 +0800

    Update link.js

--- service/link.js
+++ service/link.js
@@ -10,7 +10,7 @@ import crypto from 'node:crypto';
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 function generatorHash(url) {
   var md5 = crypto.createHash('md5');
