b029c12f5cd633817f5bc2835714650c1d8c91ef
mifuo <110472388+mifuo@users.noreply.github.com>
2025-11-16 13:16:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true
       },
-      '/u': {
+      '/': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
